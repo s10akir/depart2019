16998e0f2349f2f33d189aadb8d079bf584b4a67
Akira Shinohara <k017c1067@it-neec.jp>
2019-08-22 23:58:42 +0900

    [add] ログイン機能の実装

--- backend/routes/api/v1/user.js
+++ backend/routes/api/v1/user.js
@@ -1,6 +1,47 @@
+require('../../../models');
+
 const { Router } = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
 
 const router = Router();
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.find({ id: username }, (err, doc) => {
+    // TODO: need refactoring
+    if (err !== null) {
+      return done({ err: 'database error.' });
+    }
+
+    if (doc.length === 0) {
+      return done({ err: 'user not found.' });
+    }
+
+    if (doc[0].password !== password) {
+      return done({ err: 'login failed.' });
+    }
+
+    return done(null, username);
+  });
+}));
+
+passport.serializeUser((id, done) => {
+  done(null, id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.find({ id }, (error, doc) => {
+    if (error) {
+      return done(error);
+    }
+    return done(null, doc[0]);
+  });
+});
 
 /**
  * user.idをキーにユーザ情報を取得する
@@ -32,9 +73,9 @@ router.delete('/:id', (req, res) => {
 /**
  * サービスへのログインを行う
  */
-router.post('/login', (req, res) => {
+router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({
-    status: 'err'
+    status: 'login success.'
   });
 });
 
