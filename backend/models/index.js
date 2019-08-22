b5673db4a36eb3382706830e9e6a5ea233411dec
Akira Shinohara <k017c1067@it-neec.jp>
2019-08-22 15:11:42 +0900

    [fix] typo

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/depart', { NewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/depart', { useNewUrlParser: true });
