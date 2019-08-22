2a0cd88a70a3f4653f6f2d0e4d622dc5ffcce3a1
monomo123 <nkura0nobuk@gmail.com>
2019-08-22 15:30:32 +0900

    ユーザーのホームとログイン画面の変更

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import UserLogin from './views/user-login.vue'
+import UserHome from './views/user-home.vue'
 
 Vue.use(Router)
 
@@ -11,15 +12,13 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: UserHome
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/user-login.vue/',
+      name: 'login',
+      component: UserLogin
+
     }
   ]
 })
