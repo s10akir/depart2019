14912abdec901a05cb3e37038d1171fe007123b2
monomo123 <nkura0nobuk@gmail.com>
2019-08-22 16:47:26 +0900

    ハンバーガーメニューの実装

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import UserLogin from './views/user-login.vue'
 import UserHome from './views/user-home.vue'
+import UserParent from './views/user-parent.vue'
 
 Vue.use(Router)
 
@@ -19,6 +20,11 @@ export default new Router({
       name: 'login',
       component: UserLogin
 
+    },
+    {
+      path: '/user-parent.vue/',
+      name: 'parent',
+      component: UserParent
     }
   ]
 })
