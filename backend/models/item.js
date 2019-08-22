8051586dabf6afd11e7c564e696c5266fe0bafe4
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2019-08-22 17:29:58 +0900

    [new] モデル作成

---
+++ backend/models/item.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Item = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  count: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  original_price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  best_by: {  // 賞味期限
+    type: Date
+  },
+  use_by: {  // 消費期限
+    type: Date
+  },
+  published_until: {  // 掲載期限
+    type: Date,
+    required: true
+  },
+  image: {  // 商品写真のurl
+    type: String
+  },
+  created_at: {
+    type: Date,
+    required: true
+  },
+  updated_at: {
+    type: Date,
+    required: true
+  }
+});
+
+mongoose.model('Item', Item);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-  id: String,
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    required: true
+  },
+  updated_at: {
+    type: Date,
+    required: true
+  }
 });
 
 mongoose.model('User', User);
