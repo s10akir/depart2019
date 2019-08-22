cb1763473df3ff76ddef87d0013811491844e9d5
Akira Shinohara <k017c1067@it-neec.jp>
2019-08-23 01:49:53 +0900

    [add] store authenticator

--- backend/routes/api/v1/store.js
+++ backend/routes/api/v1/store.js
@@ -1,13 +1,68 @@
+require('../../../models');
+
 const { Router } = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 const router = Router();
+
+const Store = mongoose.model('Store');
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use(new LocalStrategy((username, password, done) => {
+  Store.find({ username }, (err, doc) => {
+    // TODO: need refactoring
+    if (err !== null) {
+      return done({ err: 'database error.' });
+    }
+
+    if (doc.length === 0) {
+      return done({ err: 'user not found.' });
+    }
+
+    if (doc[0].password !== password) {
+      return done({ err: 'login failed.' });
+    }
+
+    return done(null, username);
+  });
+}));
+
+passport.serializeUser((username, done) => {
+  done(null, username);
+});
+
+passport.deserializeUser((username, done) => {
+  Store.find({ username }, (error, doc) => {
+    if (error) {
+      return done(error);
+    }
+    return done(null, doc[0]);
+  });
+});
 
 /**
  * store.idをキーにストア情報を取得する
  */
 router.get('/:id', (req, res) => {
-  res.json({
-    status: 'err'
+  Store.find({ username: req.params.id }, (err, doc) => {
+    if (err) {
+      res.json({
+        err: 'database error.'
+      });
+    } else if (doc.length === 0) {
+      res.json({
+        err: 'store not found.'
+      });
+    } else {
+      const store = delete doc[0].password;
+      res.json(
+        store
+      );
+    }
   });
 });
 
