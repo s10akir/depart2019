fb68074bddf970cad13dfc571793f82250285405
Nejomo <daisanbukan@gmail.com>
2019-08-23 14:38:25 +0900

    ルーティングをrouter-linkからVue側で行うサンプルを一つ作成

--- frontend/src/views/user/login.vue
+++ frontend/src/views/user/login.vue
@@ -2,18 +2,44 @@
 	<div id="app">
 		<h1>FOOSHEL</h1>
 		<h2>ユーザーとしてのログイン</h2>
-		<label>ユーザーID</label>
+		<label>{{userId}}</label>
 		<input type="text"><br>
-		<label>パスワード</label>
+		<label>{{password}}</label>
 		<input type="text"><br>
-		<router-link to="/user/main">
-			<button type="button" class="btn btn-outline-primary">
+			<button type="button" class="btn btn-outline-primary" v-on:click="login">
 				Login
 			</button>
-		</router-link>
 	</div>
 </template>
+<script>
+import axios from 'axios'
+export default{
+  data:function() {
+  return {
+    userId:"",
+    password:""
+    }
+  },
+  methods:{
+    login () {
+      axios.post('/api/v1/user/login/',{"username":"abc","password":"abc123"}).then((response) => {
+        if(response.data.status=='login success.')
+        {
+          alert('login success');
+          this.$router.push('/user/main')
+        }
+        else
+        {
+          alert('login failed.');
+        }
+      }, (error) => {
+        alert(error);
+      })
+    }
+  }
+}
 
+</script>
 
 <style>
 </style>
